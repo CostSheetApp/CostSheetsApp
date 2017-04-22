f3aaec1ce1a66408c4c369552dfc19867e4e3f05
Alex Omar Fernandez Lorenzo <olorenzo@outlook.com>
2017-04-22 15:33:34 -0600

    add layouts

---
+++ src/layouts/empty-layout.js
@@ -0,0 +1,32 @@
+import React, {Component,PropTypes} from 'react';
+//import '../styles/empty-layout.css';
+import { Layout } from 'antd';
+const { Content,Footer } = Layout;
+
+
+class emptyLayout extends Component {
+  state = {
+    collapsed: false,
+  };
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+  render() {
+    return (
+      <Layout style={{ height: '100%'}}>
+          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+            {this.props.children}
+          </Content>
+           <Footer>footer</Footer>
+      </Layout>
+    );
+  }
+}
+
+emptyLayout.propTypes = {
+    children: PropTypes.object,
+};
+
+export default emptyLayout;
---
+++ src/layouts/main-layout.js
@@ -0,0 +1,82 @@
+import React,{Component,PropTypes} from 'react';
+import '../styles/main-layout.css';
+import { Layout, Menu, Icon } from 'antd';
+import {push} from 'react-router-redux';
+import {connect} from 'react-redux';
+const { Header, Sider, Content } = Layout;
+
+
+class mainLayout extends Component {
+  state = {
+    collapsed: false,
+  };
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+  render() {
+    let {changeLocationTo}=this.props;
+    return (
+      <Layout style={{ height: '100%'}}>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['/']} onClick={({ key }) => changeLocationTo(key)}>
+            <Menu.Item key="/">
+              <Icon type="pie-chart" />
+              <span className="nav-text">dashboard</span>
+            </Menu.Item>
+            <Menu.Item key="/appoitments">
+              <Icon type="calendar" />
+              <span className="nav-text">Appoitments</span>
+            </Menu.Item>
+            <Menu.Item key="/patients" >
+              <Icon type="usergroup-add" />
+              <span className="nav-text">Patients</span>
+            </Menu.Item>
+            <Menu.Item key="/reports">
+              <Icon type="copy" />
+              <span className="nav-text">Reports</span>
+            </Menu.Item>
+            <Menu.Item key="/finances">
+              <Icon type="credit-card" />
+              <span className="nav-text">Finances</span>
+            </Menu.Item>
+            
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+            {this.props.children}
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+mainLayout.propTypes = {
+    children: PropTypes.object,
+    changeLocationTo:PropTypes.func,
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeLocationTo: (location) => {
+      dispatch(push(location));
+    }
+  };
+};
+
+export default connect(null,mapDispatchToProps)(mainLayout);
