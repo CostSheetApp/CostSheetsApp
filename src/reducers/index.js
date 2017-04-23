6fc72bad9df516b6a6d296f6986c905711cb0edc
Alex Omar Fernandez Lorenzo <olorenzo@outlook.com>
2017-04-23 17:14:05 -0600

    Setup Route, Global API URL constant, add reducer account to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import fuelSavings from './fuelSavingsReducer';
 import {routerReducer} from 'react-router-redux';
+import accountReducer from './accountReducer';
 
 const rootReducer = combineReducers({
   fuelSavings,
-  routing: routerReducer
+  routing: routerReducer,
+  account: accountReducer
 });
 
 export default rootReducer;
