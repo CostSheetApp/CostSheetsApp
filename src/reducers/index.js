b45935cebc464bdd7154bc0e73eadc3fda93dfb2
Carlos Luis <carlosluis_lopez@yahoo.com>
2017-06-08 16:26:08 -0600

    Mano de Obra
    Cambios iniciales de Mano de Obra

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,6 +9,7 @@ import entityReducer from './entityReducer';
 import costSheetsReducer from './costSheetsReducer';
 import projectReducer from './projectsReducer';
 import toolReducer from './toolsReducer';
+import manPowerReducer from './manPowersReducer';
 
 const rootReducer = combineReducers({
   fuelSavings,
@@ -20,7 +21,8 @@ const rootReducer = combineReducers({
   users: usersReducer,
   costSheets: costSheetsReducer,
   projects: projectReducer,
-  tools: toolReducer
+  tools: toolReducer,
+  manPowers: manPowerReducer
 });
 
 export default rootReducer;
